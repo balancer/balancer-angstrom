980ae728a31d2f8d23bcdfb0c48b10fe20a34c99
João Bruno <joaobrunoah@gmail.com>
2025-09-02 15:52:42 -0300

    Add Angstrom Router

---
+++ contracts/AngstromRouter.sol
@@ -0,0 +1,175 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.24;
+
+import { IPermit2 } from "permit2/src/interfaces/IPermit2.sol";
+
+import { IWETH } from "@balancer-labs/v3-interfaces/contracts/solidity-utils/misc/IWETH.sol";
+import { IRouter } from "@balancer-labs/v3-interfaces/contracts/vault/IRouter.sol";
+import { IVault } from "@balancer-labs/v3-interfaces/contracts/vault/IVault.sol";
+
+import { BatchRouterHooks } from "@balancer-labs/v3-vault/contracts/BatchRouterHooks.sol";
+
+// TODO interface
+contract AngstromRouter is BatchRouterHooks {
+    uint32 private _lastUnlockTimestamp;
+
+    constructor(
+        IVault vault,
+        IWETH weth,
+        IPermit2 permit2,
+        string memory routerVersion
+    ) BatchRouterHooks(vault, weth, permit2, routerVersion) {
+        // solhint-disable-previous-line no-empty-blocks
+    }
+
+    /***************************************************************************
+                                       Swaps
+    ***************************************************************************/
+
+    /// @inheritdoc IBatchRouter
+    function swapExactIn(
+        SwapPathExactAmountIn[] memory paths,
+        uint256 deadline,
+        bool wethIsEth,
+        bytes calldata userData
+    )
+        external
+        payable
+        saveSender(msg.sender)
+        returns (uint256[] memory pathAmountsOut, address[] memory tokensOut, uint256[] memory amountsOut)
+    {
+        // Unlocks the router in this block. If the router is already unlocked, reverts.
+        _unlockRouter();
+
+        return
+            abi.decode(
+                _vault.unlock(
+                    abi.encodeCall(
+                        BatchRouterHooks.swapExactInHook,
+                        SwapExactInHookParams({
+                            sender: msg.sender,
+                            paths: paths,
+                            deadline: deadline,
+                            wethIsEth: wethIsEth,
+                            userData: userData
+                        })
+                    )
+                ),
+                (uint256[], address[], uint256[])
+            );
+    }
+
+    /// @inheritdoc IBatchRouter
+    function swapExactOut(
+        SwapPathExactAmountOut[] memory paths,
+        uint256 deadline,
+        bool wethIsEth,
+        bytes calldata userData
+    )
+        external
+        payable
+        saveSender(msg.sender)
+        returns (uint256[] memory pathAmountsIn, address[] memory tokensIn, uint256[] memory amountsIn)
+    {
+        // Unlocks the router in this block. If the router is already unlocked, reverts.
+        _unlockRouter();
+
+        return
+            abi.decode(
+                _vault.unlock(
+                    abi.encodeCall(
+                        BatchRouterHooks.swapExactOutHook,
+                        SwapExactOutHookParams({
+                            sender: msg.sender,
+                            paths: paths,
+                            deadline: deadline,
+                            wethIsEth: wethIsEth,
+                            userData: userData
+                        })
+                    )
+                ),
+                (uint256[], address[], uint256[])
+            );
+    }
+
+    /***************************************************************************
+                                     Queries
+    ***************************************************************************/
+
+    /// @inheritdoc IBatchRouterQueries
+    function querySwapExactIn(
+        SwapPathExactAmountIn[] memory paths,
+        address sender,
+        bytes calldata userData
+    )
+        external
+        saveSender(sender)
+        returns (uint256[] memory pathAmountsOut, address[] memory tokensOut, uint256[] memory amountsOut)
+    {
+        for (uint256 i = 0; i < paths.length; ++i) {
+            paths[i].minAmountOut = 0;
+        }
+
+        return
+            abi.decode(
+                _vault.quote(
+                    abi.encodeCall(
+                        BatchRouterHooks.querySwapExactInHook,
+                        SwapExactInHookParams({
+                            sender: address(this),
+                            paths: paths,
+                            deadline: type(uint256).max,
+                            wethIsEth: false,
+                            userData: userData
+                        })
+                    )
+                ),
+                (uint256[], address[], uint256[])
+            );
+    }
+
+    /// @inheritdoc IBatchRouterQueries
+    function querySwapExactOut(
+        SwapPathExactAmountOut[] memory paths,
+        address sender,
+        bytes calldata userData
+    )
+        external
+        saveSender(sender)
+        returns (uint256[] memory pathAmountsIn, address[] memory tokensIn, uint256[] memory amountsIn)
+    {
+        for (uint256 i = 0; i < paths.length; ++i) {
+            paths[i].maxAmountIn = _MAX_AMOUNT;
+        }
+
+        return
+            abi.decode(
+                _vault.quote(
+                    abi.encodeCall(
+                        BatchRouterHooks.querySwapExactOutHook,
+                        SwapExactOutHookParams({
+                            sender: address(this),
+                            paths: paths,
+                            deadline: type(uint256).max,
+                            wethIsEth: false,
+                            userData: userData
+                        })
+                    )
+                ),
+                (uint256[], address[], uint256[])
+            );
+    }
+
+    function _unlockRouter() internal {
+        if (_isRouterUnlocked()) {
+            revert RouterAlreadyUnlocked();
+        }
+
+        _lastUnlockTimestamp = uint32(block.timestamp);
+    }
+
+    function _isRouterUnlocked() internal view returns (bool) {
+        return _lastUnlockTimestamp == block.timestamp;
+    }
+}
